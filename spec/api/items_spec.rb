ab5edd1373e272bf4e6003083069ac763604e522
Jarod Adair <jarod.adair@manheim.com>
2015-11-25 19:43:28 -0600

    revert 98603c79 move to api folder

--- spec/api/items_spec.rb
+++ spec/api/items_spec.rb
@@ -1,52 +1,57 @@
-# require_relative '../spec_helper'
-#
-# describe 'Items API' do
-#   it "sends a list of items" do
-#     location = FactoryGirl.create(:location)
-#     FactoryGirl.create_list(:item, 5, location_id: location.id)
-#
-#     get "/api/v1/locations/#{location.id}/items"
-#     json = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(json['items'].length).to eq(5)
-#   end
-#
-#   it "sends an item by id" do
-#     location = FactoryGirl.create(:location)
-#     item = FactoryGirl.create(:item, location_id: location.id)
-#
-#     get "/api/v1/locations/#{location.id}/items/#{item.id}"
-#
-#     expect(response).to be_success
-#     expect(json).to eql location.to_json
-#   end
-#
-#   it "creates an item" do
-#     location = FactoryGirl.create(:location)
-#     item = FactoryGirl.build(:item, location_id: location.id)
-#
-#     post "/api/v1/locations/#{location.id}/items", body: item
-#
-#     expect(response).to be_success
-#   end
-#
-#   it "updates an item" do
-#     location = FactoryGirl.create(:location)
-#     item = FactoryGirl.create(:item, location_id: location.id)
-#     item.name = 'Updated Name'
-#
-#     put "/api/v1/locations/#{location.id}/items/#{item.id}", body: location
-#
-#     expect(response).to be_success
-#   end
-#
-#   it "deletes an item" do
-#     location = FactoryGirl.create(:location)
-#     item = FactoryGirl.create(:item, location_id: item)
-#
-#     delete "/api/v1/locations/#{location.id}/items/#{item.id}"
-#
-#     expect(response).to be_success
-#   end
-# end
+require_relative '../rails_helper'
+require_relative '../spec_helper'
+
+describe 'Locations API' do
+  it "sends a list of items for a location" do
+    location = FactoryGirl.create(:location)
+    FactoryGirl.create_list(:item, 15, location_id: location.id)
+
+    get "/api/v1/locations/#{location.id}/items"
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(JSON.parse(response.body).size).to eq(15)
+  end
+
+  it "sends a item by id" do
+    location = FactoryGirl.create(:location)
+    item = FactoryGirl.create(:item, location_id: location.id)
+
+    get "/api/v1/locations/#{location.id}/items/#{item.id}"
+
+    expect(response).to be_success
+    expect(response.body).to eql item.to_json
+  end
+
+  it "creates an item" do
+    location = FactoryGirl.create(:location)
+    item = FactoryGirl.build(:item, location_id: location.id)
+    body = {
+        name: item.name,
+        description: item.description
+    }.to_json
+
+    post "/api/v1/locations/#{location.id}/items", body: body
+
+    expect(response).to be_success
+  end
+
+  it "updates an item" do
+    location = FactoryGirl.create(:location)
+    item = FactoryGirl.create(:item, location_id: location.id)
+    item.name = 'Updated Item'
+
+    put "/api/v1/locations/#{location.id}/items/#{item.id}", body: location.to_json
+
+    expect(response).to be_success
+  end
+
+  it "deletes an item" do
+    location = FactoryGirl.create(:location)
+    item = FactoryGirl.create(:item, location_id: location.id)
+
+    delete "/api/v1/locations/#{location.id}/items/#{item.id}"
+
+    expect(response).to be_success
+  end
+end
