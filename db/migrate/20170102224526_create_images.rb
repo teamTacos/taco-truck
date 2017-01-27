0c15099a1a21661ab50ce3dec1bfee5b1113e2a1
Wallace Harwood <wallace.harwood@manheim.com>
2017-01-27 10:30:27 -0600

    [wallace] fixing images migration

--- db/migrate/20170102224526_create_images.rb
+++ db/migrate/20170102224526_create_images.rb
@@ -5,6 +5,9 @@ class CreateImages < ActiveRecord::Migration
       t.integer :location_id
       t.integer :item_id
       t.integer :review_id
+      t.integer :location_banner
+      t.integer :review_banner
+      t.integer :item_banner
     end
   end
 end
