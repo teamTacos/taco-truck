04e956a3561efb0b38244f3310d8e88356aaf7b0
Jarod Adair <jarod.adair@manheim.com>
2017-01-27 16:24:38 -0600

    rebuild item table schema migration

--- db/migrate/20151122171355_create_items.rb
+++ db/migrate/20151122171355_create_items.rb
@@ -4,6 +4,7 @@ class CreateItems < ActiveRecord::Migration
       t.integer :location_id
       t.string :name
       t.text :description
+      t.string :thumbnail
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127161839) do
+ActiveRecord::Schema.define(version: 20170127215420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20170127161839) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "created_by"
   end
 
   create_table "locations", force: :cascade do |t|
