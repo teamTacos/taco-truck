ed04b823b2f1b8c7af54c2ec458956522c1ed13f
Wallace Harwood <wallace.harwood@manheim.com>
2017-02-09 14:09:48 -0600

    [wallace] removing access token from users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208140910) do
+ActiveRecord::Schema.define(version: 20170209200813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 20170208140910) do
 
   create_table "users", force: :cascade do |t|
     t.string "fb_user_id"
-    t.string "access_token"
     t.string "email"
     t.string "first_name"
     t.string "last_name"
